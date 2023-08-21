09b97a134822d1b531e0b341c4e57aca767701a2
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-08-21 15:02:29 +0200

    add apicontroller

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function projectsList()
+    {
+        $projects = Project::all();
+
+        return response()->json([
+            'listProjects' => $projects
+        ]);
+    }
+}
