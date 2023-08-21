a61b9e9983e4da367207f1bf18943257a57391ed
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-08-21 17:02:22 +0200

    push

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
+
 class ApiController extends Controller
 {
     public function projectsList()
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::prefix('/v1')->group(function () {
 
