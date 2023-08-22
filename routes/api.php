c33efd81ab0cf39f0242617eb1d621cd70151022
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-08-22 15:55:10 +0200

    push

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::prefix('/v1')->group(function () {
     Route::get('/projectsList', [ProjectController::class, 'projectsList']);
 });
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
